1a37e5760822325304ab23cc778f0dadc7e3adee
gauravk0247 <gauravkhairnar2106@gmail.com>
2023-10-16 07:09:05 +0530

    Library Project

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,17 @@
 function windowOpen(){
     window.open("sign-in-out.html");
 }
+
+// function pdf(){
+//     window.open(".Book/writings-near-the-end-of-the-human-erastories-and-book-reviews-obooko.pdf");
+// }
+
+function showSignUpForm() {
+document.getElementById("signin-form").style.display = "none";
+document.getElementById("signup-form").style.display = "block";
+}
+
+function showSignInForm() {
+document.getElementById("signup-form").style.display = "none";
+document.getElementById("signin-form").style.display = "block";
+}
