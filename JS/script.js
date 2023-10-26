d475b6a7250295df8edf81b803c6e189d63d406b
gauravk0247 <gauravkhairnar2106@gmail.com>
2023-10-26 21:00:19 +0530

    Book Library

--- JS/script.js
+++ JS/script.js
@@ -2,12 +2,13 @@ function windowOpen(){
     window.open("sign-in-out.html");
 }
 
+function paymentWindow(){
+    window.open("payment.html");
+}
+
 function windowOpen1(){
     window.open("book_1.html");
 }
-// function pdf(){
-//     window.open(".Book/writings-near-the-end-of-the-human-erastories-and-book-reviews-obooko.pdf");
-// }
 
 function showSignUpForm() {
 document.getElementById("signin-form").style.display = "none";
@@ -18,3 +19,57 @@ function showSignInForm() {
 document.getElementById("signup-form").style.display = "none";
 document.getElementById("signin-form").style.display = "block";
 }
+
+// Cart
+let cart = [];
+function addToCart(bookName, price) {
+    const existingItem = cart.find(item => item.bookName === bookName);
+  
+    if (existingItem) {
+      existingItem.price += price;
+    } else {
+      const item = {
+        bookName: bookName,
+        price: price,
+      };
+      cart.push(item);
+    }
+  
+    updateCartUI();
+  }
+  
+function updateCartUI() {
+    const cartContainer = document.getElementById("cart");
+  
+    const cartList = document.createElement("ul");
+    cartList.id = "cart-list";
+  
+    let totalPrice = 0;
+  
+    for (const item of cart) {
+      totalPrice += item.price;
+      const listItem = document.createElement("li");
+      listItem.textContent = `${item.bookName} - Rs.${item.price.toFixed(2)}`;
+      cartList.appendChild(listItem);
+    }
+  
+    const totalElement = document.createElement("p");
+    totalElement.textContent = `Total: Rs.${totalPrice.toFixed(2)}`;
+  
+    cartContainer.innerHTML = "";
+    cartContainer.appendChild(cartList);
+    cartContainer.appendChild(totalElement);
+  }
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const paymentForm = document.getElementById("payment-form");
+  
+    if (paymentForm) {
+      const submitButton = document.getElementById("submit-payment");
+  
+      submitButton.addEventListener("click", function (e) {
+        e.preventDefault();
+        alert("Payment successful!");
+      });
+    }
+  });
